8b8ab6bbcb6c5cc032fcf637429117cdf84edca7
Richard Abishai <rickoshade1891@gmail.com>
2024-12-28 12:13:42 +0530

    Add financial and web search agents with environment variable support

---
+++ financial_agent.py
@@ -0,0 +1,46 @@
+from phi.agent import Agent
+from phi.model.groq import Groq
+from phi.tools.yfinance import YFinanceTools
+from phi.tools.duckduckgo import DuckDuckGo
+import openai
+
+import os
+from dotenv import load_dotenv
+load_dotenv()
+openai.api_key=os.getenv("OPENAI_API_KEY")
+
+## web search agent
+web_search_agent=Agent(
+    name="Web Search Agent",
+    role="Search the web for the information",
+    model=Groq(id="llama3-groq-70b-8192-tool-use-preview"),
+    tools=[DuckDuckGo()],
+    instructions=["Alway include sources"],
+    show_tools_calls=True,
+    markdown=True,
+
+)
+
+## Financial agent
+finance_agent=Agent(
+    name="Finance AI Agent",
+    model=Groq(id="llama3-groq-70b-8192-tool-use-preview"),
+    tools=[
+        YFinanceTools(stock_price=True, analyst_recommendations=True, stock_fundamentals=True,
+                      company_news=True),
+    ],
+    instructions=["Use tables to display the data"],
+    show_tool_calls=True,
+    markdown=True,
+
+)
+
+multi_ai_agent=Agent(
+    team=[web_search_agent,finance_agent],
+    instructions=["Always include sources","Use table to display the data"],
+    show_tool_calls=True,
+    markdown=True,
+)
+
+multi_ai_agent.print_response("Summarize analyst recommendation and share the latest news for NVDA",stream=True)
+
---
+++ playground.py
@@ -0,0 +1,48 @@
+import openai
+from phi.agent import Agent
+import phi.api
+from phi.model.openai import OpenAIChat
+from phi.tools.yfinance import YFinanceTools
+from phi.tools.duckduckgo import DuckDuckGo
+from dotenv import load_dotenv
+from phi.model.groq import Groq
+
+import os
+import phi
+from phi.playground import Playground, serve_playground_app
+# Load environment variables from .env file
+load_dotenv()
+
+phi.api=os.getenv("PHI_API_KEY")
+
+## web search agent
+web_search_agent=Agent(
+    name="Web Search Agent",
+    role="Search the web for the information",
+    model=Groq(id="llama3-groq-70b-8192-tool-use-preview"),
+    tools=[DuckDuckGo()],
+    instructions=["Alway include sources"],
+    show_tools_calls=True,
+    markdown=True,
+
+)
+
+## Financial agent
+finance_agent=Agent(
+    name="Finance AI Agent",
+    model=Groq(id="llama3-groq-70b-8192-tool-use-preview"),
+    tools=[
+        YFinanceTools(stock_price=True, analyst_recommendations=True, stock_fundamentals=True,
+                      company_news=True),
+    ],
+    instructions=["Use tables to display the data"],
+    show_tool_calls=True,
+    markdown=True,
+
+)
+
+app=Playground(agents=[finance_agent,web_search_agent]).get_app()
+
+if __name__=="__main__":
+    serve_playground_app("playground:app",reload=True)
+
